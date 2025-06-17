641e348c6c4b06b08ba26cd02e41788907e20945
poornima200631 <poornimatewatia48@gmail.com>
2025-06-17 18:34:29 +0530

    Describe what you changed

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function bgcolor(color){
+function bgcolor(color,className){
   document.body.style.backgroundColor=`${color}`
+  const a=document.getElementsByClassName(`${className}`)
+  const ele= document.getElementById(`${color}`)
+  for (let i = 0; i < a.length; i++) {
+    a[i].style.fontWeight = "normal";
+    a[i].style.fontSize = "12px";
+    
+
+  }
+
+  
+  if (ele) {
+    ele.style.fontWeight = "bold";
+    ele.style.fontSize="20px"
+    ele.style.alignItems = "center";
+    ele.style.justifyContent = "center"
    
+  }
+ 
 }
+
